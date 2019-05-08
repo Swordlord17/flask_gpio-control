d7f90183622f4b212cc91ba10e17b7b38542f9da
Swordlord17 <darkslayer12527@gmail.com>
2019-05-08 15:47:36 +0000

    Add files via upload
    cd to the folder 'flas.py' is stored 
    export it as flask app
    run flask by 'python -m flask run --host'0.0.0.0' '

---
+++ flas.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as G
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/servo")
+def servo():
+    servoPIN = 17
+    G.setmode(G.BCM)
+    G.setup(servoPIN, G.OUT)
+    p = G.PWM(servoPIN, 50)
+    p.start(2.5)
+    try:
+        while True:
+            p.ChangeDutyCycle(5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(7.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(10)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(12.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(10)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(7.5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(5)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(2.5)
+            time.sleep(0.5)
+            
+    except KeyboardInterrupt:
+        
+            p.stop()
+            G.cleanup()
+
+app.run(debug=True, host='0.0.0.0')
+            
+
+            
+     
+
+    
